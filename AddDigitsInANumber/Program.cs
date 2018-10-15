7d90cc5ebf93a7375073e3c8c411dde26116d83e
zmil44 <zachary.j.miller@outlook.com>
2018-10-15 15:30:00 -0400

    completed

--- AddDigitsInANumber/Program.cs
+++ AddDigitsInANumber/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace AddDigitsInANumber
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(AddDigits(input));
+        }
+        public static int AddDigits(int number)
+        {
+            int sum = 0;
+            if (number < 10 && number > 0)
+            {
+                return number;
+            }
+            else
+            {
+                while (number != 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+                return AddDigits(sum);
+            }
         }
     }
 }
